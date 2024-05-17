c81f34eb8ccd25bdd03539dc16fe5a647029251c
Daniel <daniel.schmidt89@freenet.de>
2024-05-17 17:25:25 +0200

    Changes

--- src/DataTableComponent.php
+++ src/DataTableComponent.php
@@ -18,22 +18,25 @@ abstract class DataTableComponent extends Component
 
     public function getData(): Collection
     {
-        $searchFields = [];
-        foreach ($this->columns() as $c) {
-            if ($c->isSearchable()) {
-                $searchFields[] = $c->getField();
-            }
-        }
+//        $searchFields = [];
+//        foreach ($this->columns() as $c) {
+//            if ($c->isSearchable()) {
+//                $searchFields[] = $c->getField();
+//            }
+//        }
 
-        if (count($searchFields) > 0) {
-            return $this->model::whereAny($searchFields, 'LIKE', '%' . $this->searchInput . '%')
-                ->get();
-        }
+//        if (count($searchFields) > 0) {
+//            return $this->model::whereAny($searchFields, 'LIKE', '%' . $this->search . '%')
+//                ->get();
+//        }
         return $this->model::get();
     }
 
     public function render(): \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
     {
-        return view('dataTable::datatable');
+        return view('dataTable::datatable', [
+            'columns' => $this->columns(),
+            'data' => $this->getData(),
+        ]);
     }
 }
