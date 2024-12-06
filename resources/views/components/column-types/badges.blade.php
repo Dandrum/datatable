56fc91a05ad6ff201bca9a5abf66d393c14ae101
Daniel <daniel.schmidt89@freenet.de>
2024-12-06 21:11:26 +0100

    change color badges

--- resources/views/components/column-types/badges.blade.php
+++ resources/views/components/column-types/badges.blade.php
@@ -2,6 +2,27 @@
 
 @php($value = $this->getFieldData($column->getField(), $data))
 @if($value)
-    <div
-        class="mr-2 rounded bg-{{ $column->getColor($value) }}-900 py-1 text-xs font-medium text-slate-200 px-2.5">{{__($value)}}</div>
+    @switch($column->getColor($value))
+        @case('red')
+            <div class="mr-2 rounded bg-red-900 py-1 text-xs font-medium text-slate-200 px-2.5">{{__($value)}}</div>
+        @break
+        @case('yellow')
+            <div class="mr-2 rounded bg-yellow-900 py-1 text-xs font-medium text-slate-200 px-2.5">{{__($value)}}</div>
+            @break
+        @case('blue')
+            <div class="mr-2 rounded bg-blue-900 py-1 text-xs font-medium text-slate-200 px-2.5">{{__($value)}}</div>
+            @break
+        @case('green')
+            <div class="mr-2 rounded bg-green-900 py-1 text-xs font-medium text-slate-200 px-2.5">{{__($value)}}</div>
+            @break
+        @case('purple')
+            <div class="mr-2 rounded bg-purple-900 py-1 text-xs font-medium text-slate-200 px-2.5">{{__($value)}}</div>
+            @break
+        @case('fuchsia')
+            <div class="mr-2 rounded bg-fuchsia-900 py-1 text-xs font-medium text-slate-200 px-2.5">{{__($value)}}</div>
+            @break
+        @default
+            <div class="mr-2 rounded bg-slate-900 py-1 text-xs font-medium text-slate-200 px-2.5">{{__($value)}}</div>
+            @break
+    @endswitch
 @endif
