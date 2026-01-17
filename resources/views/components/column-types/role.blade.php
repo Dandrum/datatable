a23ea7ee665cabf229d8376ceb07f568427d3bc6
Daniel <daniel.schmidt89@freenet.de>
2026-01-17 23:52:17 +0100

    fix color

--- resources/views/components/column-types/role.blade.php
+++ resources/views/components/column-types/role.blade.php
@@ -1,13 +1,13 @@
 @if($this->getFieldData($column->getField(), $data))
     @foreach($this->getFieldData($column->getField(), $data) as $role)
         <span
-            class="bg-green-100 text-white text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
+            class="bg-green-600 text-white text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
             {{$role}}
         </span>
     @endforeach
 @else
     <span
-        class="bg-green-100 text-white text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
+        class="bg-green-600 text-white text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
         {{__('Guest')}}
     </span>
 @endif
